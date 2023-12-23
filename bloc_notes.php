a92b2e1c770a76a6554a4c39b52c231db6e42d02
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-23 22:57:11 +0100

    block_notes

--- bloc_notes.php
+++ bloc_notes.php
@@ -1,91 +1,52 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="fr"><head>
+<!DOCTYPE html>
+<html lang="fr">
 
-
-
-
-
-
-
-
-
-
-
-
-  
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Bresse-Ping--Bloc-notes</title>
-  
-
-  
-  
+<head>
+  <meta charset="UTF-8">
+  <title>
+    Bresse Ping
+  </title>
+  <link rel="shortcut icon" type="image/ico" href="http://www.bresse-ping.com/favicon.ico">
   <meta content="Thierry Lachaux" name="author">
+  <meta content="Club sportif de tennis de table, ping-pong Ã  Louhans Chateaurenaud, en Bresse, affiliÃ© Ã  FFTT"
+    name="description">
+  <meta name="keywords"
+    content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
+  <link rel="stylesheet" href="css/base.css">
+  <script src="https://kit.fontawesome.com/d559253348.js" crossorigin="anonymous"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-29967241-1');
+  </script>
+</head>
 
-  
-  <meta content="Club sportif de tennis de table, ping-pong à Louhans Chateaurenaud, en Bresse, affilié à FFTT" name="description">
+<body>
+  <table class="fond_principal" style="min-height:100vh; width: 100%;  text-align: left; border: 0;" cellpadding="2"
+    cellspacing="0">
+    <tbody>
+      <tr>
+        <td style="vertical-align: top;">
+          <?php
+          include("cartouche.php");
+          include("menu.php");
+          ?>
+          <hr style="width: 100%; height: 2px;">
+          <div id="titre_page">Bloc Notes<br>
+            <br>
+          </div>
+          Vous pouvez retrouver les dates des championnats et autres tournois et
+          stages sur le site du <a href="http://www.cd71tt.com" target="_blank">CD71</a>.
+          <br>
+          <span style="text-decoration: underline;">Pour les inscriptions,
+            merci de voir avec le Bureau.</span><br>
+          <br>
+          <hr style="width: 100%; height: 2px;">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
 
-  
-  <meta name="keywords" content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
-
-  
-  <link rel="stylesheet" href="css/base.css" type="text/css">
-
-  
-  <script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29967241-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  </script></head><body>
-<table class="fond_principal" style="text-align: left; width: 80%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="0">
-
-  <tbody>
-    <tr>
-      <td style="vertical-align: top;"><?php include ("cartouche.php");
-
-
-
-			include ("date_heure.php");
-
-
-
-			include ("menu.php");
-
-
-
-		?>
-      <hr style="width: 100%; height: 2px;">
-      <div id="titre_page">Bloc Notes<br>
-      <br>
-      </div>
-Vous pouvez retrouver les dates des championnats et autres tournois et
-stages sur le site du <a href="http://www.cd71tt.com" target="_blank">CD71</a>.
-      <br>
-      <span style="text-decoration: underline;">Pour les inscriptions,
-merci de voir avec le Bureau.</span><br>
-      <br>
-      <div style="text-align: center;">
-      <hr style="width: 100%; height: 2px;"><br>
-      <span style="font-style: italic;"></span><br>
-      <br>
-      <br><br>
-      <br>
-      <br>
-      <br>
-      </div>
-      </td>
-    </tr>
-  </tbody>
-</table>
-
-<br>
-
-<br>
-
-</body></html>
+</html>
