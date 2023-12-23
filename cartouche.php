4d543802d60b7666555ecfd9da59c53726d831cf
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-23 18:07:27 +0100

    cartouche

--- cartouche.php
+++ cartouche.php
@@ -1,12 +1,16 @@
-<table style="text-align: left; width: 100%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
-        <tbody>
-          <tr>
-            <td style="vertical-align: middle; text-align: center; width: 20%;"><img style="width: 200px; height: 128px;" alt="" src="css/logo_gauche.JPG"><br>
-            </td>
-            <td style="vertical-align: middle; text-align: center; width: 60%;"><img style="width: 718px; height: 144px;" alt="" src="css/bandeau.png"><br>
-            </td>
-            <td style="vertical-align: middle; text-align: center; width: 20%;"><img style="width: 300px; height: 115px;" alt="" src="css/titre2.png">
-            </td>
-          </tr>
-        </tbody>
-      </table>
+<table style="text-align: left; width: 100%; margin-left: auto; margin-right: auto; border: 0;" cellpadding="2"
+  cellspacing="2">
+  <tbody>
+    <tr>
+      <td style="vertical-align: middle; text-align: center; width: 20%;"><img style="width: 200px; height: 128px;"
+          alt="" src="css/logo_gauche.JPG"><br>
+      </td>
+      <td style="vertical-align: middle; text-align: center; width: 60%;"><img style="width: 718px; height: 144px;"
+          alt="" src="css/bandeau.png"><br>
+      </td>
+      <td style="vertical-align: middle; text-align: center; width: 20%;"><img style="width: 300px; height: 115px;"
+          alt="" src="css/titre2.png">
+      </td>
+    </tr>
+  </tbody>
+</table>
