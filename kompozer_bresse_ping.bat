c199c990ae99673c0c18cec4a9e6a7c712dd520f
Kantin-Petit <kantin.dev@gmail.com>
2023-12-23 18:05:55 +0100

    Remove unnecessary files and code

---
+++ kompozer_bresse_ping.bat
@@ -0,0 +1,37 @@
+echo on
+
+ren bloc_notes.php bloc_notes.html
+ren cartouche.php cartouche.html
+ren club.php club.html
+ren competition.php competition.html
+ren competition_gpj.php competition_gpj.html
+ren competition_gpd.php competition_gpd.html
+ren competition_champ_equipe.php competition_champ_equipe.html
+ren competition_autres.php competition_autres.html
+ren contact.php contact.html
+ren contact_retour.php contact_retour.html
+ren index.php index.html
+ren liens_utiles.php liens_utiles.html
+ren menu.php menu.html
+ren presse.php presse.html
+ren tarifs.php tarifs.html
+ren fitpingtonic.php fitpingtonic.html
+
+"e:\program files\KompoZer 0.8b3\kompozer.exe"
+
+ren bloc_notes.html bloc_notes.php 
+ren cartouche.html cartouche.php 
+ren club.html club.php
+ren competition.html competition.php
+ren competition_gpj.html competition_gpj.php 
+ren competition_gpd.html competition_gpd.php 
+ren competition_champ_equipe.html competition_champ_equipe.php
+ren competition_autres.html competition_autres.php
+ren contact.html contact.php
+ren contact_retour.html contact_retour.php
+ren index.html index.php
+ren liens_utiles.html liens_utiles.php
+ren menu.html menu.php
+ren presse.html presse.php
+ren tarifs.html tarifs.php
+ren fitpingtonic.html fitpingtonic.php
