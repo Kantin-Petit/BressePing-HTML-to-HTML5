0e2643a77752bfca13691b61f62f8df5fa42407b
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-23 22:52:50 +0100

    babyping

--- babyping.php
+++ babyping.php
@@ -1,181 +1,212 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="fr"><head>
+<!DOCTYPE html>
+<html lang="fr">
 
-
-
-
-  
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Bresse-Ping -- Baby Ping</title>
-  
-
-  
-  
+<head>
+  <meta charset="UTF-8">
+  <title>
+    Bresse Ping
+  </title>
+  <link rel="shortcut icon" type="image/ico" href="http://www.bresse-ping.com/favicon.ico">
   <meta content="Thierry Lachaux" name="author">
+  <meta content="Club sportif de tennis de table, ping-pong Ã  Louhans Chateaurenaud, en Bresse, affiliÃ© Ã  FFTT"
+    name="description">
+  <meta name="keywords"
+    content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
+  <link rel="stylesheet" href="css/base.css">
+  <script src="https://kit.fontawesome.com/d559253348.js" crossorigin="anonymous"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-29967241-1');
+  </script>
+</head>
 
-  
-  <meta content="Club sportif de tennis de table, ping-pong à Louhans Chateaurenaud, en Bresse, affilié à FFTT" name="description">
+<body>
+  <table class="fond_principal" style="text-align: left; margin-left: auto; margin-right: auto; border:0"
+    cellpadding="2" cellspacing="0">
+    <tbody>
+      <tr>
+        <td style="vertical-align: top;">
+          <?php
+          include("cartouche.php");
+          include("menu.php");
+          ?>
+          <hr style="width: 100%; height: 2px;">
+          <div id="titre_page">
+            BABY-PING
+            <br>
+            <br>
+          </div>
+          <br>
+          <span style="font-weight: bold; font-style: italic; text-decoration: underline;">
+            Le mercredi 28 octobre 2015 journÃ©e stage avec Malory Lasnier pour nos tout petits
+          </span>
+          <br>
+          <br>
+          <br>
+          A l'image des licenciÃ©s adultes du club, nos baby ont vraiment apprÃ©ciÃ© cette belle journÃ©e de stage ping avec
+          Malory.<br>
+          JournÃ©e Ã© la fois ludique et dÃ©tente pour nos petites tÃªtes blondes mais aussi technique pour leurs inculquer
+          les gestes de base pour la bonne pratique de notre sport prÃ©fÃ©rÃ©!!!<br>
+          Chacun s'est rÃ©joui de passer ainsi un moment agrÃ©able tous ensemble et en compagnie de cette formidable
+          coach....<br>
+          Vivement le prochain stage.<br>
+          <br>
+          <br>
+          <table style="text-align: left; width: 80%; margin-left: auto; margin-right: auto; border:0;" cellpadding="2"
+            cellspacing="2">
+            <tbody>
+              <tr>
+                <td style="vertical-align: top;">
+                  <img style="width: 304px; height: 229px;" alt="stage Malaury"
+                    src="babyping/stage%20malaury%20%281%29.JPG">
+                </td>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%282%29.JPG">
+                </td>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%283%29.JPG">
+                </td>
+              </tr>
+              <tr>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%284%29.JPG">
+                </td>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%285%29.JPG">
+                </td>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%286%29.JPG">
+                </td>
+              </tr>
+              <tr>
+                <td style="vertical-align: top;">
+                  <img style="width: 304px; height: 228px;" alt="stage malaury"
+                    src="babyping/stage%20malaury%20%287%29.JPG">
+                </td>
+                <td style="vertical-align: top;">
+                  <img style="width: 300px; height: 225px;" alt="stage malory"
+                    src="babyping/stage%20malaury%20%288%29.JPG">
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br>
+          <br>
+          <div style="text-align: left;">
+            <img style="width: 172px; height: 79px;" alt="label Baby-Ping" title="label Baby-Ping"
+              src="babyping/PING%204%207%20ANS.JPG">
+            <span style="font-size: larger;">
+              <span style="font-size: larger;">
+                <span style="font-size: larger;">
+                  <span style="font-size: larger;">
+                    <span style="font-size: larger;">
+                      <span style="font-size: larger;">
+                        L
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+            e baby-ping a dÃ©marrÃ© Ã  Bresse-Ping durant la saison 2013-2014, sous l'impulsion de notre ATD Yoann
+            Guillemin et encadrÃ© par la FÃ©dÃ©ration, qui a initiÃ© l'action "Premier Pas Pongiste". Cette action a amenÃ©
+            Yoann, ainsi que Christophe et Florian, dans les Ã©coles maternelles de Ratte, Bantanges, Branges, Louhans
+            (Stella), ChÃ¢teaurenaud et Sainte-Croix afin de faire dÃ©couvrir aux enfants de 4 Ã  7 ans notre sport
+            prÃ©fÃ©rÃ©. Ces demi-journÃ©es de dÃ©couverte ont alors permis aux enfants de venir participer aux sÃ©ances
+            encadrÃ©es du mercredi aprÃ¨s-midi. Florian, Patrice et Thierry leur ont ainsi pu faire dÃ©couvrir des
+            activitÃ©s ludiques autour du Ping, toujours dans la bonne humeur!<br>
+            Les sÃ©ances de Baby-Ping ont continuÃ© la saison suivante, avec une vingtaine d'enfants (le plus petit avait
+            Ã  peine 4 ans!). Cela a permis Ã  Bresse-Ping de dÃ©crocher le
+            <span style="font-weight: bold; font-style: italic;">label Ping 4-7 ans</span>
+            , en remplissant les critÃ¨res d'attribution.<br>
+            Cette annÃ©e, une intervention de Florian et Christophe a eu lieu Ã  la crÃªche Hola Kids de Branges mardi 20
+            octobre, 11 enfants y ont participÃ©, sous l'oeil bienveillant de la directrice Emilie Vinck.<br>
+            <div style="text-align: center;">
+              <img style="width: 450px; height: 305px;" alt="Bresse-Ping Ã  Holakids" title="Bresse-Ping Ã  Holakids"
+                src="babyping/holakids.png">
+            </div>
+            <br>
+            <span style="font-weight: bold; font-style: italic; text-decoration: underline;">
+              Le Baby Ping, sÃ©ances chaque mercredi de 18h Ã  18h45 depuis le 23 septembre 2015.
+            </span>
+            <br>
+            <br>
+            <span style="font-weight: bold; font-style: italic;">
+              Un veritable moment de bonheur..........
+            </span>
+            <br>
+            <br>
 
-  
-  <meta name="keywords" content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
+            Vous vous demandez bien ce que peuvent faire vos tout petits pendant la sÃ©ance de baby ping qui leur est
+            dÃ©diÃ©e et dont ils sont les principaux acteurs!!<br>
 
-  
-  <link rel="stylesheet" href="css/base.css" type="text/css">
+            Et bien voyez par vous mÃªme avec ces quelques photos bien souriantes et gaies.Voyez comment ils arrivent Ã 
+            faire tourner en rond nos chers entraineurs animateurs que sont Florian et Patrice. Une faÃ§on ludique
+            d'enseigner dÃ¨s le plus jeune Ã¢ge les bases du tennis de table Ã  nos petits. Que du bonheur.... pour les
+            petits et les grands.....<br>
+            C'est pas la sÃ©ance pendant laquelle on s'ennuie !!!! <br>
+            Vous poserez la question aux entraineurs.<br>
+            <br>
+            <table style="text-align: left; width: 90%; height: 294px; margin-left: auto; margin-right: auto; border:0;"
+              cellpadding="2" cellspacing="2">
+              <tbody>
+                <tr>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 397px; height: 231px;" alt=""
+                      src="photos/WP_20150923_18_26_42_Pro.jpg">
+                  </td>
+                  <td style="vertical-align: top;">
+                    <img style="width: 300px; height: 168px;" alt="" src="photos/WP_20150923_18_22_09_Pro.jpg">
+                  </td>
+                </tr>
+                <tr>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 300px; height: 168px;" alt=""
+                      src="photos/WP_20150923_18_22_27_Pro.jpg">
+                  </td>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 300px; height: 168px;" alt=""
+                      src="photos/WP_20150923_18_21_58_Pro.jpg">
+                  </td>
+                </tr>
+                <tr>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 300px; height: 168px;" alt=""
+                      src="photos/WP_20150923_18_21_48_Pro.jpg">
+                  </td>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 200px; height: 112px;" alt=""
+                      src="photos/WP_20150923_18_36_42_Pro.jpg">
+                  </td>
+                </tr>
+                <tr>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 400px; height: 225px;" alt=""
+                      src="photos/WP_20150923_18_33_37_Pro.jpg">
+                  </td>
+                  <td style="vertical-align: top;">
+                    <img style="border: 0px solid ; width: 350px; height: 196px;" alt=""
+                      src="photos/WP_20150923_18_22_09_Pro.jpg">
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <br>
+            <br>
+          </div>
 
-  
-  <script type="text/javascript">
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29967241-1']);
-  _gaq.push(['_trackPageview']);
+</body>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  </script></head><body>
-<table class="fond_principal" style="text-align: left; width: 80%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="0">
-
-  <tbody>
-    <tr>
-      <td style="vertical-align: top;"><?php include ("cartouche.php");
-			include ("date_heure.php");
-			include ("menu.php");
-		?>
-      <hr style="width: 100%; height: 2px;">
-      <div id="titre_page">BABY-PING<br>
-      <br>
-      </div>
-      <br>
-      <span style="font-weight: bold; font-style: italic; text-decoration: underline;">Le mercredi 28 octobre 2015 journée stage avec Malory Lasnier pour nos tout petits</span><br>
-      <br>
-      <br>
-A l'image des licenciés adultes du club, nos baby ont vraiment apprécié cette belle journée de stage ping avec Malory.<br>
-Journée à la fois ludique et détente pour nos petites têtes blondes
-mais aussi technique pour leurs inculquer les gestes de base pour la
-bonne pratique de notre sport préféré!!!<br>
-Chacun s'est réjoui de passer ainsi un moment agréable tous ensemble et en compagnie de cette formidable coach....<br>
-Vivement le prochain stage.<br>
-      <br>
-      <br>
-      <table style="text-align: left; width: 80%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
-        <tbody>
-          <tr>
-            <td style="vertical-align: top;"><img style="width: 304px; height: 229px;" alt="stage Malaury" src="babyping/stage%20malaury%20%281%29.JPG"></td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malaury" src="babyping/stage%20malaury%20%282%29.JPG"><br>
-            </td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malaury" src="babyping/stage%20malaury%20%283%29.JPG"><br>
-            </td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malaury" src="babyping/stage%20malaury%20%284%29.JPG"><br>
-            </td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malaury" src="babyping/stage%20malaury%20%285%29.JPG"><br>
-            </td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malaury" src="babyping/stage%20malaury%20%286%29.JPG"><br>
-            </td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><img style="width: 304px; height: 228px;" alt="stage malaury" src="babyping/stage%20malaury%20%287%29.JPG"><br>
-            </td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 225px;" alt="stage malory" src="babyping/stage%20malaury%20%288%29.JPG"><br>
-            </td>
-            <td style="vertical-align: top;"><br>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <br>
-<br>
-            <div style="text-align: left;"><img style="width: 172px; height: 79px;" alt="label Baby-Ping" title="label Baby-Ping" src="babyping/PING%204%207%20ANS.JPG">
-      <big><big><big><big><big><big>L</big></big></big></big></big></big>e baby-ping a démarré à Bresse-Ping durant la saison 2013-2014, sous
-l'impulsion de notre ATD Yoann Guillemin et encadré par la Fédération,
-qui a initié l'action "Premier Pas Pongiste". Cette action a amené
-Yoann, ainsi que Christophe et Florian, dans les écoles maternelles de
-Ratte, Bantanges, Branges, Louhans (Stella), Châteaurenaud et
-Sainte-Croix afin de faire découvrir aux enfants de 4 à 7 ans notre
-sport préféré. Ces demi-journées de découverte ont alors permis aux
-enfants de venir&nbsp; participer aux séances encadrées du mercredi
-après-midi. Florian, Patrice et Thierry leur ont ainsi pu faire
-découvrir des activités ludiques autour du Ping, toujours dans la bonne
-humeur! <br>
-      Les séances de Baby-Ping ont continué la saison
-suivante, avec une vingtaine d'enfants (le plus petit avait à peine 4
-ans!). Cela a permis à Bresse-Ping de décrocher le <span style="font-weight: bold; font-style: italic;">label Ping 4-7 ans</span>, en remplissant les critères d'attribution.<br>
-Cette année, une intervention de Florian et Christophe a eu lieu à la
-crêche Hola Kids de Branges mardi 20 octobre, 11 enfants y ont
-participé, sous l'oeil bienveillant de la directrice Emilie Vinck. <br>
-      <div style="text-align: center;"><img style="width: 450px; height: 305px;" alt="Bresse-Ping à Holakids" title="Bresse-Ping à Holakids" src="babyping/holakids.png"><br>
-      </div>
-      <br>
-      <span style="font-weight: bold; font-style: italic; text-decoration: underline;">Le Baby Ping, séances chaque mercredi de 18h à 18h45 depuis le 23 septembre 2015.</span><br>
-
-      <br>
-
-      <span style="font-weight: bold; font-style: italic;">Un veritable moment de bonheur.</span><span style="font-weight: bold;">.........</span><br style="font-weight: bold; font-style: italic; text-decoration: underline;">
-
-      <br>
-
-Vous vous demandez bien ce que peuvent faire vos tout petits pendant la
-séance de baby ping qui leur est dédiée et dont ils sont les principaux
-acteurs!!<br>
-
-Et bien voyez par vous même avec ces quelques photos bien souriantes et
-gaies.Voyez comment ils arrivent à faire tourner en rond nos chers
-entraineurs animateurs que sont Florian et Patrice. Une façon ludique
-d'enseigner dès le plus jeune âge les bases du tennis de table à nos
-petits. Que du bonheur.... pour les petits et les grands.....<br>
-
-C'est pas la séance pendant laquelle on s'ennuie !!!!&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; <br>
-
-Vous poserez la question aux entraineurs.<br>
-      <br>
-      <table style="text-align: left; width: 90%; height: 294px; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
-
-        <tbody>
-          <tr>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 397px; height: 231px;" alt="" src="photos/WP_20150923_18_26_42_Pro.jpg"></td>
-            <td style="vertical-align: top;"><img style="width: 300px; height: 168px;" alt="" src="photos/WP_20150923_18_22_09_Pro.jpg"></td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 300px; height: 168px;" alt="" src="photos/WP_20150923_18_22_27_Pro.jpg"></td>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 300px; height: 168px;" alt="" src="photos/WP_20150923_18_21_58_Pro.jpg"></td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 300px; height: 168px;" alt="" src="photos/WP_20150923_18_21_48_Pro.jpg"></td>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 200px; height: 112px;" alt="" src="photos/WP_20150923_18_36_42_Pro.jpg"></td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 400px; height: 225px;" alt="" src="photos/WP_20150923_18_33_37_Pro.jpg"></td>
-            <td style="vertical-align: top;"><img style="border: 0px solid ; width: 350px; height: 196px;" alt="" src="photos/WP_20150923_18_22_09_Pro.jpg"></td>
-          </tr>
-        </tbody>
-      
-      </table>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-<br>
-      </div>
-      
-      </td>
-    </tr>
-  </tbody>
-</table>
-
-<br>
-
-<br>
-
-</body></html>
+</html>
