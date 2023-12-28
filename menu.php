291e8038d7a9a69374fc0aa861e12d16f36438b7
Kantin-Petit <kantin.dev@gmail.com>
2023-12-28 21:24:22 +0100

    menu v2

--- menu.php
+++ menu.php
@@ -1,32 +1,47 @@
 <hr style="width: 100%; height: 2px;">
 <table style="text-align: left; width: 100%; border: 0;" class="menu" cellpadding="2" cellspacing="2">
-  <tbody>
-    <tr>
-      <td style="vertical-align: top; text-align: center; width: 11%;"><a href="index.php"><img
-            style="border: 0px solid ; " alt="Accueil" src="css/menu_accueil.png"></a><br>
-      </td>
-      <td style="vertical-align: top; text-align: center; width: 11%;"><a href="club.php"><img
-            style="border: 0px solid ; " alt="Club" src="css/menu_club.png"></a><br>
-      </td>
-      <td style="vertical-align: top; width: 11%; text-align: center;"><a href="tarifs.php"><img
-            style="border: 0px solid ; " alt="Tarifs" src="css/menu_tarifs.png"></a><br>
-      </td>
-      <td style="vertical-align: top; width: 11%; text-align: center;">
-        <a href="competition.php"><img style="border: 0px solid ; " alt="Les comp�titions"
-            src="css/menu_competition.png"></a>
-      </td>
-      <td style="vertical-align: top; width: 11%; text-align: center;"><a href="bloc_notes.php"><img
-            style="border: 0px solid ;" alt="Bloc notes" src="css/menu_blocnotes.png"></a>
-      </td>
-      <td style="vertical-align: top; text-align: center; width: 11%;"><a href="babyping.php"><img
-            style="border: 0px solid ; " alt="menu Baby-Ping" title="menu Baby-Ping"
-            src="css/menu_baby.png"></a>&nbsp;<br>
-      </td>
-      <td style="vertical-align: top; width: 11%; text-align: center;"><a href="liens_utiles.php"><img
-            style="border: 0px solid ; " alt="Liens utiles" src="css/menu_liens.png"></a>&nbsp;
-      </td>
-      <td style="vertical-align: top; width: 11%; text-align: center;"><a href="contact.php"><img
-            style="border: 0px solid ; " alt="Contact" src="css/menu_contact.png"></a></td>
-    </tr>
-  </tbody>
+      <tbody>
+            <tr>
+                  <td style="vertical-align: top; text-align: center; width: 11%;">
+                        <a href="index.php">
+                              <img style="border: 0px solid ; " alt="Accueil" src="css/menu_accueil.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; text-align: center; width: 11%;">
+                        <a href="club.php">
+                              <img style="border: 0px solid ; " alt="Club" src="css/menu_club.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; width: 11%; text-align: center;">
+                        <a href="tarifs.php">
+                              <img style="border: 0px solid ; " alt="Tarifs" src="css/menu_tarifs.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; width: 11%; text-align: center;">
+                        <a href="competition.php">
+                              <img style="border: 0px solid ; " alt="Les compétitions" src="css/menu_competition.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; width: 11%; text-align: center;">
+                        <a href="bloc_notes.php">
+                              <img style="border: 0px solid ;" alt="Bloc notes" src="css/menu_blocnotes.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; text-align: center; width: 11%;">
+                        <a href="babyping.php">
+                              <img style="border: 0px solid ; " alt="menu Baby-Ping" src="css/menu_baby.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; width: 11%; text-align: center;">
+                        <a href="liens_utiles.php">
+                              <img style="border: 0px solid ; " alt="Liens utiles" src="css/menu_liens.png">
+                        </a>
+                  </td>
+                  <td style="vertical-align: top; width: 11%; text-align: center;">
+                        <a href="contact.php">
+                              <img style="border: 0px solid ; " alt="Contact" src="css/menu_contact.png">
+                        </a>
+                  </td>
+            </tr>
+      </tbody>
 </table>
