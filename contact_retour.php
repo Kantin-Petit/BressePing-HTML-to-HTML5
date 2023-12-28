945f792759cc3b4bb2757660cb6dd6f2ba4aefd2
Kantin-Petit <kantin.dev@gmail.com>
2023-12-28 19:57:35 +0100

    contact_retour

--- contact_retour.php
+++ contact_retour.php
@@ -1,53 +1,64 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="fr"><head>
+<html lang="fr">
 
-
-
-
-  
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Bresse Ping-Tennis Table Louhans - formulaire contact</title>
-  
-
-  
-  
+<head>
+  <meta charset="UTF-8">
+  <title>
+    Bresse Ping
+  </title>
+  <link rel="shortcut icon" type="image/ico" href="http://www.bresse-ping.com/favicon.ico">
   <meta content="Thierry Lachaux" name="author">
+  <meta content="Club sportif de tennis de table, ping-pong Ã  Louhans Chateaurenaud, en Bresse, affiliÃ© Ã  FFTT"
+    name="description">
+  <meta name="keywords"
+    content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
+  <link rel="stylesheet" href="css/base.css">
+  <script src="https://kit.fontawesome.com/d559253348.js" crossorigin="anonymous"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-29967241-1');
+  </script>
+</head>
 
-  
-  <meta content="Club sportif de tennis de table, ping-pong à Louhans Chateaurenaud, en Bresse, affilié à FFTT" name="description">
+<body>
+  <table class="fond_principal" style="text-align: left; width: 100%; margin-left: auto; margin-right: auto;"
+    cellpadding="2" cellspacing="0">
 
-  
-  <meta name="keywords" content="tennis de table, ping-pong, louhans, bresse, oms, chateaurenaud, club sportif, gymnase, lycee agricole, fftt, lbtt">
+    <tbody>
+      <tr>
+        <td style="vertical-align: top;">
+          <?php
+          include("cartouche.php");
+          include("menu.php");
+          ?>
+          <hr style="width: 100%; height: 2px;">
+          <br>
+          <br>
+          <div style="text-align: center; font-weight: bold; color: rgb(153, 0, 0);">
+            <span style="font-size: larger; text-decoration: underline; font-style: italic;">
+              <span style="font-size: larger;">
+                <span style="font-size: larger;">
+                  <span style="font-size: larger;">
+                    Merci pour votre message!
+                  </span>
+                </span>
+              </span>
+            </span>
+            <br>
+          </div>
+          <br>
+          <div style="text-align: center;">
+            <a href="index.php">Retour accueil</a>
+          </div>
+          <br>
+          <br>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
-  
-  <link rel="stylesheet" href="css/base.css" type="text/css"></head><body>
-<table class="fond_principal" style="text-align: left; width: 80%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="0">
+</body>
 
-  <tbody>
-    <tr>
-      <td style="vertical-align: top;">
-		
-		<?php include ("cartouche.php");
-			include ("date_heure.php");
-			include ("menu.php");
-		?>
-       
-      <hr style="width: 100%; height: 2px;">&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br>
-      <br>
-      <div style="text-align: center; font-weight: bold; color: rgb(153, 0, 0);"><big style="text-decoration: underline; font-style: italic;"><big><big><big>Merci pour votre message!</big></big></big></big><br>
-      </div>
-      <br>
-      <div style="text-align: center;"><a href="index.php">Retour accueil</a><br>
-      </div>
-      <br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</td>
-    </tr>
-  </tbody>
-</table>
-
-<br>
-
-<br>
-
-</body></html>
+</html>
