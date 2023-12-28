b9f228c42f541d1db2c3cad4777d33d2240635c0
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-28 20:11:40 +0100

    form2mail

--- form2mail.php
+++ form2mail.php
@@ -1,29 +1,18 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html><head>
-<?php $message="Votre nom: ".$HTTP_POST_VARS["nom"]."\n";
+<html>
 
+<head>
+    <?php
+    $message = "Votre nom: " . $HTTP_POST_VARS["nom"] . "\n";
+    $message = $message . "Votre prenom: " . $HTTP_POST_VARS["prenom"] . "\n";
+    $message = $message . "Votre e-mail : " . $HTTP_POST_VARS["e-mail"] . "\n\n";
+    $message = $message . "Votre message:\n\n" . $HTTP_POST_VARS["message"];
+    mail("bressepinglouhans71@gmail.com", "Message du site", "$message");
+    header('Location:contact_retour.php');
+    ?>
+</head>
 
-$message=$message."Votre prenom: ".$HTTP_POST_VARS["prenom"]."\n";
+<body>
+</body>
 
-
-$message=$message."Votre e-mail : ".$HTTP_POST_VARS["e-mail"]."\n\n";
-
-
-$message=$message."Votre message:
-
-
-\n\n".$HTTP_POST_VARS["message"];
-
-
-mail("bressepinglouhans71@gmail.com","Message du site","$message");
-
-
-header('Location:contact_retour.php');
-
-
-?>
-</head><body>
-<br>
-
-<br>
-</body></html>
+</html>
