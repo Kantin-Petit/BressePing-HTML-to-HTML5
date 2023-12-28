d2b27ca11d4329f7fb5a09de4d8d4a51235c0675
Kantin-Petit <kantin.dev@gmail.com>
2023-12-28 19:53:34 +0100

    compteur

--- compteur.php
+++
@@ -1,6 +0,0 @@
-<html>
-</sc<!-- Debut du code Domino Counter -->
-<script type="text/javascript">var dominocounter_a = "bresseping71";</script>
-<script type="text/javascript" src="http://www.dominocounter.net/webcounter.js"></script>
-<!-- Fin du code Domino Counter --> Visiteurs
-</html>
